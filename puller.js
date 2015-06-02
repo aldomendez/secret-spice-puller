4425226cf40b8c9878d2d7336fe9ad0fdfdc0395
Aldo Mendez <aldomendez86@gmail.com>
2015-06-01 22:46:14 -0500

    Ya se mantiene ejecutando, guarda la respuesta en disco

--- puller.js
+++ puller.js
@@ -1,20 +1,69 @@
-var EventLogger = require('node-windows').EventLogger;
-var log = new EventLogger('Hello World');
+//var EventLogger = require('node-windows').EventLogger;
+//var log = new EventLogger('Hello World');
 var rest = require('unirest');
-log.info('Service for updating monitors working');
+var fs = require('fs');
+//log.info('Service for updating monitors working');
 
-var interval;
+var a, callRest, running, timeoutTime, writeFile;
 
-interval =(function() {
-      unirest.get('http://cymautocert/osaapp/semaforo-dev/toolbox.php?action=updateTables');
+running = false;
+
+timeoutTime = 50000;
+
+a = function(timeout) {
+  var interval;
+  if (!running) {
+    return interval = (function(_this) {
+      return function() {
+        return setTimeout(callRest, timeout);
+      };
+    })(this)();
+  }
+};
+
+callRest = function() {
+  running = true;
+  console.log('Running call');
+  return rest.get('http://cymautocert/osaapp/semaforo-dev/toolbox.php?action=updateTables').end(function(response) {
+    running = false;
+    console.log('response geted');
+    writeFile('C:\\apps\\node-win\\response.txt', JSON.stringify(response));
+    return a(timeoutTime);
+  });
+};
+
+writeFile = function(address, content) {
+  return fs.writeFile(address, content, function(err) {
+    if (err) {
+      console.log(error);
+      return console.log('File saved');
     }
- , 5000);
+  });
+};
 
+a(timeoutTime);
 
 /*
-request
-interval = do ->
- return setInterval ()=>
-  request = unirest.get('http://cymautocert/osaapp/semaforo-dev/toolbox.php?action=updateTables')
- ,5000
- */
+running = false
+timeoutTime = 50000
+a = (timeout)->
+ if not running
+  interval = do =>setTimeout(callRest,timeout)
+
+callRest = ->
+ running = true
+ console.log 'Running call'
+ rest.get('http://cymautocert/osaapp/semaforo-dev/toolbox.php?action=updateTables').end (response)->
+  running = false
+  console.log 'response geted'
+  writeFile('C:\\apps\\node-win\\response.txt', JSON.stringify response)
+  a timeoutTime
+
+writeFile = (address, content)->
+ fs.writeFile address, content, (err)->
+  if err
+   console.log error
+   console.log 'File saved'
+
+a timeoutTime
+*/
